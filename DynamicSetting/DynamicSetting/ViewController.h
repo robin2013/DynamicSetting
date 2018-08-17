a2a9c209ef6b8741bf60384b27d23ddaab08bb40
cuiyinjiang <cuiyinjiang@cuiyinjiangdeMacBook-Pro.local>
2018-08-17 19:30:45 +0800

    initial
    Signed-off-by: cuiyinjiang <cuiyinjiang@cuiyinjiangdeMacBook-Pro.local>

---
+++ DynamicSetting/DynamicSetting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DynamicSetting
+//
+//  Created by cuiyinjiang on 2018/8/17.
+//  Copyright © 2018年 cuiyinjiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DynamicSetting/DynamicSetting/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DynamicSetting
+//
+//  Created by cuiyinjiang on 2018/8/17.
+//  Copyright © 2018年 cuiyinjiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
